93de81a499c310a1041eb303cbfaa5bb12e8e3cb
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-10-12 20:18:21 +0200

    add project directory

--- Tasks/Yarn/yarnTask.ts
+++ Tasks/Yarn/yarnTask.ts
@@ -4,13 +4,22 @@ import tl = require('vsts-task-lib/task');
 
 var yarnPath = "yarn"; //path.join(__dirname, 'node_modules/.bin/yarn')
 var args = tl.getInput("Arguments");
+var path = tl.getPathInput("ProjectDirectory")
 
-async function yarnExec(){
+async function yarnExec() {
     try {
         var yarn = tl.tool(yarnPath);
         yarn.arg(args);
 
-        var result = await yarn.exec();
+        var result = await yarn.exec({
+            cwd: path,
+            env: <any>process.env,
+            silent: false,
+            failOnStdErr: undefined,
+            ignoreReturnCode: undefined,
+            outStream: undefined,
+            errStream: undefined
+        });
 
         tl.setResult(tl.TaskResult.Succeeded, "Yarn executed successfully");
 
