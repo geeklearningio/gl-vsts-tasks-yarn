58d68b5d5b4c91ef36db92e008f8a6b35f5b2d62
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-10-12 19:06:21 +0200

    yarn cannot be bundle because of current tfx restrictions

--- Tasks/Yarn/yarnTask.ts
+++ Tasks/Yarn/yarnTask.ts
@@ -2,12 +2,12 @@ import path = require('path');
 import fs = require('fs-extra');
 import tl = require('vsts-task-lib/task');
 
-
+var yarnPath = "yarn"; //path.join(__dirname, 'node_modules/.bin/yarn')
 var args = tl.getInput("Arguments");
 
 async function yarnExec(){
     try {
-        var yarn = tl.tool(path.join(__dirname, 'node_modules/.bin/yarn'));
+        var yarn = tl.tool(yarnPath);
         yarn.arg(args);
 
         var result = await yarn.exec();
