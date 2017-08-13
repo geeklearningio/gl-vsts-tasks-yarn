cb1e0e3063cc119803ca45e0f26abe81a1d9627d
Cyprien Autexier <cyprien.autexier@gmail.com>
2017-08-13 15:01:11 +0200

    Revert "fix carriage return"
    This reverts commit 9cfe7cc6f6bc9cb8a3340034cadead95e2c2c116.

--- Tasks/Yarn/npmregistry.ts
+++ Tasks/Yarn/npmregistry.ts
@@ -65,7 +65,8 @@ export class NpmRegistry implements INpmRegistry {
     public static async FromFeedId(feedId: string, authOnly?: boolean): Promise<NpmRegistry> {
         let url = NormalizeRegistry(await util.getFeedRegistryUrl(feedId));
         let nerfed = util.toNerfDart(url);
-        let auth = `always-auth=true\n${nerfed}:_authToken=${util.getSystemAccessToken()}`;
+        let auth = `always-auth=true
+                    ${nerfed}:_authToken=${util.getSystemAccessToken()}`;
 
         return new NpmRegistry(url, auth, authOnly);
     }
